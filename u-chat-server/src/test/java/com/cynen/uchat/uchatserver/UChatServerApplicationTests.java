8d8e37c0905e78da317a10585db9db8b0bde0814
cynen <cynen@foxmail.com>
2019-12-10 22:41:22 +0800

    添加后台项目

---
+++ u-chat-server/src/test/java/com/cynen/uchat/uchatserver/UChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cynen.uchat.uchatserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UChatServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
